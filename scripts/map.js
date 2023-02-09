a2e8381af2aee9a4ea1447015b078e6191239ee2
Yunus <yyurduseven@gmail.com>
2023-02-09 10:08:17 +0300

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -902,22 +902,21 @@ $(window).on('load', function() {
    */
   function changeAttribution() {
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
+    var credit = '';
     var name = getSetting('_authorName');
     var url = getSetting('_authorURL');
 
     if (name && url) {
       if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
-      credit += ' by <a href="' + url + '">' + name + '</a> | ';
+      credit += '<a href="' + url + '">' + name + '</a> | ';
     } else if (name) {
       credit += ' by ' + name + ' | ';
     } else {
       credit += ' | ';
     }
 
-    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
     if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
-    credit += ' with ';
+    credit += '';
     $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
   }
 
